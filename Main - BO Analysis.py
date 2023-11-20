e5a0b0ab8ece57a165ede503fd2383144633dc4d
Pragyan789 <agrawap8@gene.com>
2023-11-20 16:28:04 +0530

    file_name_change

--- Main - BO Analysis.py
+++ Main - BO Analysis.py
@@ -17,6 +17,7 @@
 
 import pandas as pd
 import numpy as np
+import os
 from openpyxl import load_workbook
 
 import warnings
@@ -25,6 +26,7 @@ warnings.filterwarnings('ignore')
 from datetime import datetime
 from datetime import date
 
+import DQ_Analysis_Main
 #File that contains all functions
 import bo_analysis_functions
 import dq_branch_analysis
@@ -39,14 +41,15 @@ df_input = df_input.set_index('Variable Name')
 df_input = df_input.fillna("")
 
 folder_path = list(df_input.loc['folder_path'])[0]
+supplier_folder_path = list(df_input.loc['supplier_folder_path'])[0]
 
 supplier_names_file_path = folder_path + "\\" + list(df_input.loc['supplier_names_file'])[0] + ".xlsx"
-combined_ins_path = folder_path + "\\" + list(df_input.loc['combined_ins'])[0] + ".xlsx"
-bo_file_path = folder_path + "\\" + list(df_input.loc['bo_file'])[0] + ".xlsx"
-reference_list_path = folder_path + "\\" + list(df_input.loc['reference_list'])[0] + ".xlsx"
-sap_filter_list_path = folder_path + "\\" + list(df_input.loc['sap_filter_list'])[0] + ".xlsx"
+combined_ins_path = supplier_folder_path + "\\" + list(df_input.loc['combined_ins'])[0] + ".xlsx"
+bo_file_path = supplier_folder_path + "\\" + list(df_input.loc['bo_file'])[0] + ".xlsx"
+reference_list_path = supplier_folder_path + "\\" + list(df_input.loc['reference_list'])[0] + ".xlsx"
+sap_filter_list_path = supplier_folder_path + "\\" + list(df_input.loc['sap_filter_list'])[0] + ".xlsx"
 sap_ins_file_path = folder_path + "\\" + list(df_input.loc['sap_ins_file'])[0] + ".xlsx"
-branch_report_file_path = folder_path + "\\" + list(df_input.loc['branch_report_file'])[0] + ".xlsx"
+branch_report_file_path = supplier_folder_path + "\\" + list(df_input.loc['branch_report_file'])[0] + ".xlsx"
 
 
 #Data Sources Import
@@ -118,7 +121,7 @@ supplier_name = list(df_input.loc['supplier_name'])[0]
 supplier_names_df = supplier_names_df.set_index('File Name')
 supplier_name = supplier_names_df[supplier_names_df.columns[0]][supplier_name.lower()]
 
-output_path = folder_path + "\\" + list(df_input.loc['main_file'])[0] + ".xlsx"
+output_path = supplier_folder_path + "\\" + list(df_input.loc['main_file'])[0] + ".xlsx"
 
 # Calling main analysis function
 bo_analysis_functions.bo_and_sap_analysis(ins_pivot, outs_pivot, sap_ins_pivot, supplier_name, output_path)
@@ -132,3 +135,9 @@ if df is not None and df_outs_raw is not None:
     comment = ''
     comment, branch_pivot = dq_branch_analysis.dq_non_trending_branch_analysis(branch_report_file_path,'','', output_path)
     bo_analysis_functions.unreported_branches(df_combined_ins, ins_branch_pivot, branch_pivot, output_path)
+
+DQ_Analysis_Main.comment_generation()
+
+months = {1:"JAN",2:"FEB",3:"MAR",4:"APR",5:"MAY",6:"JUN",7:"JUL",8:"AUG",9:"SEP",10:"OCT",11:"NOV",12:"DEC"}
+output_destination_path = supplier_folder_path + "\\" + supplier_name + "_" + months[(date.today().month - 1)] + str(data_month_year)[-2:] + ".xlsx"
+os.rename(output_path,output_destination_path)
